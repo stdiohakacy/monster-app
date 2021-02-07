49756c816c07fa6f4610362979ad48b3f0451744
Duy <huynhduytam2210@gmail.com>
2021-02-07 09:47:56 +0700

    Card Component

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters}/>
+                <CardList monsters={this.state.monsters} />
             </div>
         )
     }
